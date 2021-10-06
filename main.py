1f1e41f4538e62bd90913184283684473d0a41e8
lordfundarbyrd <lordfundarbyrd@gmail.com>
2021-10-06 11:02:05 -0400

    added joined boolean for error checking later

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ token = open("token.txt","r") # token in .gitignore token.txt file
 api_key = token.read()
 token.close()
 
+joined = False
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -25,17 +27,20 @@ async def on_message(message):
 
     if msg.startswith("!join"):
         vc = await music_channel().connect()
+        joined = True
 
     if msg.startswith("!leave"):
         for vc in client.voice_clients:
             if vc.guild == message.guild:
                 await vc.disconnect()
+                joined = False
 
     #if msg.startswith("$inspire"):
         #quote = get_quote()
         #await message.channel.send(quote)
 
 def music_channel():
+    # TODO: make command for setting music channel
     music_id = 895154216887259157
     music_channel = client.get_channel(music_id)
     return music_channel
