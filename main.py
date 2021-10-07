8f4bd7fe785b06c164ba2653d3ebdaac94db34d9
lordfundarbyrd <lordfundarbyrd@gmail.com>
2021-10-06 23:02:59 -0400

    comments, started adding yt implementation

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import googleapiclient.errors
 
 client = discord.Client()
 
-token = open("token.txt","r") # token in .gitignore token.txt file
-api_key = token.read()
+token = open("token.txt","r") # tokens in .gitignore token.txt file
+d_api_key = token.readline()
+yt_api_key = token.readline()
 token.close()
 
 @client.event
@@ -23,19 +24,24 @@ async def on_message(message):
 
     msg = message.content
 
+    # prints the username of the user who used the !user command
     if msg.startswith("!user"):
         await message.channel.send(message.author)
         #print(message.author.voice.channel)
 
+    # makes bot join channel user is currently in
+    #TODO: add command for setting default channel (right now message is just sent)
     if msg.startswith("!join"):
         if message.author.voice is None:
             await message.channel.send("Please join a channel, and then make me join")
         else:
             voice = discord.utils.get(client.voice_clients)
 
+            #joins channel if not already in channel
             if voice is None:
                 destination = message.author.voice.channel
                 vc = await destination.connect()
+            # disconnects from current channel and joins new channel
             else:
                 for vc in client.voice_clients:
                     if vc.guild == message.guild:
@@ -47,6 +53,9 @@ async def on_message(message):
         for vc in client.voice_clients:
             if vc.guild == message.guild:
                 await vc.disconnect()
+
+    if msg.startswith("!yt"):
+        print(msg[4:])
 
     #if msg.startswith("$inspire"):
         #quote = get_quote()
