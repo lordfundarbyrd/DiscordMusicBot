d03994e89251873f0dec5c2c31729772fbeb8d49
lordfundarbyrd <lordfundarbyrd@gmail.com>
2022-01-31 19:33:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,66 @@
 import discord
-from discord.ext import commands,tasks
 import os
+import nacl
+from discord.ext import commands,tasks
 from dotenv import load_dotenv
 import youtube_dl
+
+client = discord.Client()
+token = open("/Users/delracman/Documents/Discord/token.txt","r") # tokens in .gitignore token.txt file
+d_api_key = token.readline()
+token.close()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    # prints the username of the user who used the !user command
+    if msg.startswith("!user"):
+        await message.channel.send(message.author)
+        #print(message.author.voice.channel)
+
+    # makes bot join channel user is currently in
+    #TODO: add command for setting default channel (right now message is just sent)
+    if msg.startswith("!join"):
+        if message.author.voice is None:
+            await message.channel.send("Please join a channel, and then make me join")
+        else:
+            voice = discord.utils.get(client.voice_clients)
+
+            #joins channel if not already in channel
+            if voice is None:
+                destination = message.author.voice.channel
+                vc = await destination.connect()
+            # disconnects from current channel and joins new channel
+            else:
+                for vc in client.voice_clients:
+                    if vc.guild == message.guild:
+                        await vc.disconnect()
+                destination = message.author.voice.channel
+                vc = await destination.connect()
+
+    if msg.startswith("!leave"):
+        for vc in client.voice_clients:
+            if vc.guild == message.guild:
+                await vc.disconnect()
+
+        print(response)
+
+    #if msg.startswith("$inspire"):
+        #quote = get_quote()
+        #await message.channel.send(quote)
+
+client.run(d_api_key)
+
+
+'''
 
 load_dotenv()
 
@@ -115,4 +173,8 @@ async def stop(ctx):
         await ctx.send("The bot is not playing anything at the moment.")
 
 if __name__ == "__main__" :
-    bot.run(DISCORD_TOKEN)
+    bot.run(DISCORD_TOKEN)'''
+
+
+
+
